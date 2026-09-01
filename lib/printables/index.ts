78dc1ed71286e6b7092de5dead12619ffe01cd35
William Blacklock <wtblacklock@gmail.com>
2026-09-01 07:41:08 -0500

    feat(printables): add guide-printable resolver helpers

--- lib/printables/index.ts
+++ lib/printables/index.ts
@@ -1,2 +1,3 @@
 export type { Printable, PrintableCategoryId } from './types'
 export { PRINTABLES, getPrintableBySlug } from './data'
+export { getPrintablesForGuide, getGuidesLinkedToPrintable } from './guide-printables'
